99be5b2bf2dc83e5b7bfa7f92725b8f96e02a3e4
Syed Fazil Basheer <fazil@fazn.co>
2025-05-02 03:04:03 +0530

    Support Browser Options

--- lib/ferrum_pdf/controller.rb
+++ lib/ferrum_pdf/controller.rb
@@ -2,25 +2,27 @@ module FerrumPdf
   module Controller
     extend ActiveSupport::Concern
 
-    def render_pdf(pdf_options: {}, **rendering)
+    def render_pdf(pdf_options: {}, browser_options: {}, **rendering)
       content = render_to_string(**rendering.with_defaults(formats: [ :html ]))
 
       FerrumPdf.render_pdf(
         html: content,
         host: request.base_url + "/",
         protocol: request.protocol,
-        pdf_options: pdf_options
+        pdf_options: pdf_options,
+        browser_options: browser_options
       )
     end
 
-    def render_screenshot(screenshot_options: {}, **rendering)
+    def render_screenshot(screenshot_options: {}, browser_options: {}, **rendering)
       content = render_to_string(**rendering.with_defaults(formats: [ :html ]))
 
       FerrumPdf.render_screenshot(
         html: content,
         host: request.base_url + "/",
         protocol: request.protocol,
-        screenshot_options: screenshot_options
+        screenshot_options: screenshot_options,
+        browser_options: browser_options
       )
     end
   end
